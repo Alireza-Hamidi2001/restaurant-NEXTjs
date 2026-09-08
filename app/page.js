1527ff77b6d9c67ddb4588c757866bb7111f4ed9
Alireza-Hamidi2001 <alireza.hamidi.eng@gmail.com>
2026-09-08 17:36:23 +0330

    main page configuration

--- app/page.js
+++ app/page.js
@@ -33,7 +33,7 @@ function page() {
                 </div>
 
                 {/* Content */}
-                <div className="fixed z-10 bottom-10 left-10">
+                <div className="md:fixed p-4 md:p-0 z-10 bottom-10 left-10">
                     {/* Star Rating */}
                     <div className="animate-fade-left flex justify-start gap-1 mb-8 text-amber-400">
                         <FaStar className="w-6 h-6" />
@@ -71,7 +71,7 @@ function page() {
                     </Link>
                 </div>
                 <div
-                    className="bg-red-500 animate-scale-in fixed bottom-[45%] z-10 md:bottom-[50%] md:translate-y-[50%] md:right-20 px-4 rounded-xl md:min-h-[20rem]
+                    className="hidden md:block animate-scale-in fixed bottom-[45%] z-10 md:bottom-[50%] md:translate-y-[50%] md:right-20 px-4 rounded-xl md:min-h-[20rem]
                     w-[20rem] md:w-[25rem] border border-gray-500 backdrop-blur-[5px]">
                     <div className="relative top-0 z-20 h-[10rem] md:h-[11rem] translate-y-[-2rem] md:translate-y-[-5rem]">
                         <Image
